b93c3d286978fa755c32dbb4365b4e728da093d8
nika123w <nikusha.kurtiashvili@gmail.com>
2025-05-24 14:09:24 +0400

    Create ProductController.java

---
+++ ProductController.java
@@ -0,0 +1,14 @@
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    private final ProductService service;
+
+    public ProductController(ProductService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public Product create(@RequestBody ProductDto dto) {
+        return service.createProduct(dto);
+    }
+}
